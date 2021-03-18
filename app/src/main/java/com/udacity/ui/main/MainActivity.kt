baadfba3068f92456d70bf43569bf043b865ef70
isosic <ivan.sosic@asseco-see.hr>
2021-03-18 09:28:52 +0100

    added notification

--- app/src/main/java/com/udacity/ui/main/MainActivity.kt
+++ app/src/main/java/com/udacity/ui/main/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.udacity.ui.main
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import com.udacity.R
+import com.udacity.utils.sendNotification
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -30,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        createNotificationChannel(getString(R.string.channel_id), getString(R.string.channel_name))
 
         custom_button.setOnClickListener {
             download()
@@ -56,12 +61,30 @@ class MainActivity : AppCompatActivity() {
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
-    private fun createChanell
+    private fun createNotificationChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_LOW
+            )
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Load app status"
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            notificationManager.sendNotification(application)
+
+        }
+    }
 
     companion object {
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
-        private const val CHANNEL_ID = "channelId"
     }
 
 }
--- app/src/main/java/com/udacity/utils/NotificationUtils.kt
+++ app/src/main/java/com/udacity/utils/NotificationUtils.kt
@@ -4,7 +4,10 @@ import android.app.Application
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.udacity.R
 import com.udacity.ui.details.DetailActivity
+import com.udacity.ui.main.MainActivity
 
 private val NOTIFICATION_ID = 0
 private val REQUEST_CODE = 0
@@ -16,4 +19,19 @@ fun NotificationManager.sendNotification(application: Application) {
         application,
         NOTIFICATION_ID, intent, PendingIntent.FLAG_UPDATE_CURRENT
     )
+
+    val builder =
+        NotificationCompat.Builder(application, application.getString(R.string.channel_id))
+            .setContentTitle(application.getString(R.string.notification_title))
+            .setContentText(application.getString(R.string.notification_description))
+            .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+//            .addAction(
+//                R.mipmap.ic_launcher_round,
+//                application.getString(R.string.notification_button),
+//                pendingIntent
+//            )
+
+    notify(NOTIFICATION_ID, builder.build())
 }
